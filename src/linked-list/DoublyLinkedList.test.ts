e6d3524fb2d8e205888f3e30b31c16181cd67f48
Chandrakant <chandrakantsavvy@gmail.com>
2025-01-18 10:12:46 +0530

    add Doubly Linked List unit test

---
+++ src/linked-list/DoublyLinkedList.test.ts
@@ -0,0 +1,36 @@
+import { DLL_Node, DoublyLinkedList } from "./DoublyLinkedList";
+
+describe("Doubly Linked List", () => {
+  test("New DLL Node creation", () => {
+    const dllNode = new DLL_Node(5);
+    expect(dllNode).not.toBeNull();
+    expect(dllNode.value).toBe(5);
+    expect(dllNode.next).toBeNull();
+    expect(dllNode.previous).toBeNull();
+  });
+
+  test("New Doubly LinkedList creation", () => {
+    const dll = new DoublyLinkedList(5);
+    expect(dll).not.toBeNull();
+    expect(dll.head).not.toBeNull();
+    expect(dll.head?.value).toBe(5);
+    expect(dll.tail).not.toBeNull();
+    expect(dll.tail?.value).toBe(5);
+    expect(dll.length).toBe(1);
+  });
+
+  describe("DoublyLinkedList methods", () => {
+    let dll: DoublyLinkedList;
+
+    beforeEach(() => {
+      dll = new DoublyLinkedList(5);
+    });
+
+    test("addFirst", () => {
+      dll.addFirst(4);
+      expect(dll.length).toBe(2);
+      expect(dll.head?.value).toBe(4);
+      expect(dll.tail?.value).toBe(5);
+    });
+  });
+});
