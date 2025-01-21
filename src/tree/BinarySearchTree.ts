24eac60d50c661b87d8b4473d0b8c45fb6f0fde4
Chandrakant <chandrakantsavvy@gmail.com>
2025-01-21 20:39:56 +0530

    add Binary tree constructor

---
+++ src/tree/BinarySearchTree.ts
@@ -0,0 +1,13 @@
+class BT_Node {
+  value: number;
+  left: BT_Node | null;
+  right: BT_Node | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {}
